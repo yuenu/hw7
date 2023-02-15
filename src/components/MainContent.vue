d6e7637de8a990e7c913902fa397d92e538380e8
yuenu <miraiku82@gmail.com>
2023-02-15 23:52:51 +0800

    feat: base styles

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,26 +1,22 @@
 <script setup lang="ts">
 import IconTrash from "@/components/icons/IconTrash.vue";
-import { ref, onMounted, shallowRef } from "vue";
-import { useObjectUrl } from "@vueuse/core";
+import { ref, onMounted } from "vue";
+import Datepicker from "vue3-datepicker";
 
-export type FileEventTarget = EventTarget & { files: FileList };
+const TEXTAREAR_MAX_LENGTH = 200;
+const todoContent = ref("");
+const remainingCount = ref(0);
+const onKeyUp = () => {
+  remainingCount.value = todoContent.value.length;
+};
 
 const imgPreview = ref<HTMLImageElement | null>(null);
-
+const hasImage = ref(false);
 onMounted(() => {
-  console.log("imgPreview", imgPreview);
+  if (imgPreview.value?.src) {
+    hasImage.value = true;
+  }
 });
-
-// const loadFile = (evt: HTMLInputEvent | DragEvent) => {
-//   let files =
-//     (evt as HTMLInputEvent).target.files ||
-//     (evt as DragEvent).dataTransfer!.files;
-//   if (!files.length) return;
-//   const url = URL.createObjectURL(files[0]);
-//   imgPreview.value!.onload = function () {
-//     URL.revokeObjectURL(url);
-//   };
-// };
 
 const onFileChange = (evt: Event) => {
   const target = evt.target as HTMLInputElement;
@@ -28,9 +24,12 @@ const onFileChange = (evt: Event) => {
   if (!files || !files.length) return;
   imgPreview.value!.src = URL.createObjectURL(files[0]);
   imgPreview.value!.onload = function () {
+    hasImage.value = true;
     URL.revokeObjectURL(imgPreview.value!.src);
   };
 };
+
+const picked = ref(new Date());
 </script>
 
 <template>
@@ -43,25 +42,81 @@ const onFileChange = (evt: Event) => {
     </div>
     <div>
       <input
-        class="w-full p-4 rounded-lg bg-gray-cool"
+        class="w-full p-4 item"
         type="text"
         placeholder="Press new item title"
       />
     </div>
-    <div class="flex items-center justify-between h-32 gap-5">
-      <textarea
-        class="w-3/5 h-full p-4 rounded-lg bg-gray-cool"
-        placeholder="Content..."
-      ></textarea>
-      <div class="bg-gray-cool">
-        <input type="file" accept="image/*" @change="onFileChange" />
-        <img ref="imgPreview" />
+    <div class="flex items-center justify-between h-[30vh] gap-5">
+      <div class="relative w-3/5 h-full">
+        <textarea
+          v-model="todoContent"
+          @keyup="onKeyUp"
+          class="w-full h-full p-4 resize-none item"
+          placeholder="Content..."
+          :maxlength="TEXTAREAR_MAX_LENGTH"
+        ></textarea>
+        <span class="absolute text-sm pointer-events-none bottom-2 right-4"
+          >{{ remainingCount }} / {{ TEXTAREAR_MAX_LENGTH }}</span
+        >
+      </div>
+      <div class="relative flex-1 h-full overflow-hidden item">
+        <img
+          ref="imgPreview"
+          :class="[
+            hasImage && 'opacity-100',
+            'object-cover w-full h-full opacity-0',
+          ]"
+        />
+        <span
+          :class="[
+            hasImage && 'opacity-0',
+            'absolute -translate-x-1/2 -translate-y-1/2 select-none left-1/2 top-1/2',
+          ]"
+        >
+          Image Preview
+        </span>
       </div>
     </div>
-    <div class="flex h-10 gap-5">
-      <div class="bg-">2022/05/07</div>
+    <div class="flex items-center gap-5">
+      <div class="flex items-center w-3/5 gap-5">
+        <Datepicker
+          class="w-full h-12 text-center rounded-lg bg-gray-cool"
+          v-model="picked"
+        />
+        {{ "~" }}
+        <Datepicker
+          class="w-full h-12 text-center rounded-lg bg-gray-cool"
+          v-model="picked"
+        />
+      </div>
+      <div class="relative flex-1 h-12 rounded-lg bg-primary-100">
+        <input
+          class="upload-input"
+          type="file"
+          accept="image/*"
+          @change="onFileChange"
+        />
+        <span
+          aria-hidden="true"
+          class="absolute -translate-x-1/2 -translate-y-1/2 pointer-events-none left-1/2 top-1/2"
+        >
+          Upload Image
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.v3dp__datepicker {
+  width: 100%;
+}
+.item {
+  @apply rounded-lg bg-gray-cool;
+}
+
+.upload-input {
+  @apply z-10 w-full h-full opacity-0;
+}
+</style>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="h-screen bg-primary-200 w-[250px] px-3 py-4">
-    <h1 class="font-semibold text-lg">Demo Todo List</h1>
+  <div class="h-screen bg-primary-200 w-[250px]">
+    <h1 class="px-3 py-4 text-lg font-semibold">Demo Todo List</h1>
+    <div class="mb-6 space-y-2">
+      <div class="px-5 py-3 truncate bg-primary-300 item-active">1. Item</div>
+      <div class="px-5 py-3 truncate bg-primary-300">2. Item</div>
+    </div>
+    <button
+      type="button"
+      class="w-[200px] py-2 mx-auto text-lg bg-primary-100 rounded-xl block"
+    >
+      Add Item
+    </button>
   </div>
 </template>
+
+<style scoped lang="scss">
+.item-active {
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    border-bottom: 24px solid transparent;
+    border-top: 24px solid transparent;
+    border-right: 18px solid #fff;
+  }
+}
+</style>
